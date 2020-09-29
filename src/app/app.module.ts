ceee9839785db709b72a4420df9ce39c4d8f18ac
roodev <rodrigo.fessore@gmail.com>
2020-09-29 10:15:54 -0300

    Criando o componente navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import{ToastrModule} from "ngx-toastr"
+
+import{NavigationModule} from "./modules/navigation/navigation.module"
+import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
@@ -11,7 +13,8 @@ import{ToastrModule} from "ngx-toastr"
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    NavigationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
